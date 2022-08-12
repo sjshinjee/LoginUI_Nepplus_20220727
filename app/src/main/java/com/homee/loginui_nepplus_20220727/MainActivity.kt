7faa9c731b0fdbf7eeb0c80f6c2eb23b26fb27c2
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-08-12 23:53:23 +0900

    Nepplus로그인 화면 도전과제

---
+++ app/src/main/java/com/homee/loginui_nepplus_20220727/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.homee.loginui_nepplus_20220727
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
